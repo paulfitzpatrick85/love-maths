dc47b7643e5c3cdca49323bffab3584a4a306d22
paulfitzpatrick85 <fitzer99@gmail.com>
2022-03-04 12:23:43 +0000

    add code to generate  random numbers for question

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,16 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 })
 
+/**
+ * the main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
-
+    //generate two random whole numbers rounded down to the nearest integer
+    let num1 = Math.floor(Math.random() * 25) + 1;   // +1 stops the rounding down process for excluding 24
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
+
 
 function checkAnswer() {
 
